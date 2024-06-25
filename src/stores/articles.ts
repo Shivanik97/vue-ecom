afad86cf81a1686a0587c422a3607b089882b24c
Shivani K <shivanik.bbi18@rvce.edu.in>
2024-06-25 18:32:28 +0530

    added articles

---
+++ src/stores/articles.ts
@@ -0,0 +1,55 @@
+import { ref } from 'vue'
+
+export const articles = ref([
+  {
+    id: 1,
+    title: 'Mastering Advanced Component Patterns in Vue.js 3',
+    url: 'https://medium.com/@sumanth469/mastering-advanced-component-patterns-in-vue-js-3-5953e3f95e0d'
+  },
+  {
+    id: 2,
+    title: 'State-Driven CSS Variables in Vue 3',
+    url: 'https://medium.com/@kuntumallashivani/state-driven-css-variables-in-vue-3-fda05c28d81a'
+  },
+  {
+    id: 3,
+    title: 'Elevating Vue 3 Applications with defineAsyncComponent and Suspense',
+    url: 'https://medium.com/@revanthkumarpatha/elevating-vue-3-applications-with-defineasynccomponent-and-suspense-1ae04f5cb30f'
+  },
+  {
+    id: 4,
+    title: 'Exploring Custom Directives in Vue 3: A Practical Guide',
+    url: 'https://medium.com/@revanthkumarpatha/exploring-custom-directives-in-vue-3-a-practical-guide-921e437d00fa'
+  },
+  {
+    id: 5,
+    title:
+      'Revolutionizing Vue.js State Management: A Deep Dive into defineModel in Vue 3 Script Setup',
+    url: 'https://medium.com/@revanthkumarpatha/welcome-to-the-next-level-of-vue-js-912f0587fc3e'
+  },
+  {
+    id: 6,
+    title: 'Unleashing the Power of Pinia: Effortless State Management for Vue.js 3',
+    url: 'https://medium.com/@rajuponnamanda3239/unleashing-the-power-of-pinia-effortless-state-management-for-vue-js-3-8892772058db'
+  },
+  {
+    id: 7,
+    title: 'Pinia: The Enjoyable Vue Store',
+    url: 'https://vueschool.io/courses/pinia-the-enjoyable-vue-store'
+  },
+  {
+    id: 8,
+    title: 'Auth0 authentication in Vue 3',
+    url: 'https://medium.com/@rajuponnamanda3239/in-the-rapidly-evolving-landscape-of-web-development-securing-user-data-is-paramount-c7ea8c2a98c6'
+  },
+  {
+    id: 9,
+    title: 'How a Vue App Works Behind The Scenes',
+    url: 'https://medium.com/@venkateshkondi1533/unveiling-the-essence-of-vue-3-project-structure-9c83e049487a'
+  },
+  {
+    id: 10,
+    title: 'Vue 3 Composables for Reusable Logic',
+    url: 'https://medium.com/@venkateshkondi1533/vue-3-composables-for-reusable-logic-916b861b7532'
+  }
+])
